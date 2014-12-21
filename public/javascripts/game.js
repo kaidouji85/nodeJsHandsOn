347ea9501adee0dfc719855799433d0c53b45ad0
kaidouji85 <kaidouji85@gmail.com>
2014-12-21 16:08:49 +0900

    グーアイコンを画面に追加した。

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,20 +1,34 @@
 enchant();
+var PICT_PREFIX = '/images/';
+var PICT_JANKEN = PICT_PREFIX + 'janken.png';
 var core;
 var socket;
 var messageLabel;
+var rockSprite;
 
 window.onload = function() {
     core = new Core(320, 480);
     core.fps = 60;
     core.rootScene.backgroundColor = "black";
+
+    preLoad();
     core.onload = initGame;
     core.start();
 };
+
+function preLoad(){
+    core.preload(PICT_JANKEN);
+}
 
 function initGame(){
     messageLabel = new Label('サーバログイン中');
     messageLabel.color = 'white';
     core.rootScene.addChild(messageLabel);
+
+    rockSprite = new Sprite(128,128);
+    rockSprite.image = core.assets[PICT_JANKEN];
+    core.rootScene.addChild(rockSprite);
+
     loginServer();
 }
 
