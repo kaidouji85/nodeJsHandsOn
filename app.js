66f9c3a420a2293337c1e88f391ea4e007c0bddf
kaidouji85 <kaidouji85@gmail.com>
2014-12-21 15:35:37 +0900

    クライアントとサーバをsocket.ioで接続した。

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var httpServer;
+var io;
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(3000);
+httpServer = app.listen(3000);
+io = require('socket.io').listen(httpServer);
+
+io.sockets.on('connection',function(socket){
+    console.log('connection success.');
+});
--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,9 +1,15 @@
 enchant();
 var core;
+var socket;
 
 window.onload = function() {
     core = new Core(320, 480);
     core.fps = 60;
     core.rootScene.backgroundColor = "black";
+    core.onload = initGame;
     core.start();
 };
+
+function initGame(){
+    socket = io.connect(location.origin);
+}
